3b80a2243db469d0cdabc07aaf9603298c02f857
difico123 <ducnong0916@gmail.com>
2023-05-15 00:48:17 +0700

    write file

--- index.ts
+++ index.ts
@@ -1,10 +1,8 @@
-import { By, Key, Builder, until, WebElement } from "selenium-webdriver";
+import { Builder } from "selenium-webdriver";
 import { CrawlerService } from "./service/crawl";
-import { waiting } from "./util/helpers";
 require("chromedriver");
-
+import fs from "fs/promises";
 // import chrome from "selenium-webdriver/chrome";
-
 // const options = new chrome.Options();
 // options.addArguments("--headless");
 // options.addArguments("--disable-gpu");
@@ -16,43 +14,14 @@ async function main() {
     .build();
 
   const crawlService = new CrawlerService({ webdriver: driver });
-
-  // await driver.get("https://www.booking.com");
-
-  // const locationInputElement = await driver.findElement(By.name("ss"));
-  // await locationInputElement.sendKeys("", Key.ENTER);
-
-  // const location = await driver.findElement(By.css(".a40619bfbe"));
-  // const locationTx = await location.getText();
-
-  // await locationInputElement.sendKeys(locationTx, Key.ENTER);
-
-  // const roomListContainerElement = await driver.findElement(By.css(".d4924c9e74"))
-
-  // const roomImageElements = await roomListContainerElement.findElements(By.tagName("img"));
-
-  // const imageUrls = await Promise.all(
-  //   roomImageElements.map(async (element) => {
-  //     return await element.getAttribute("src");
-  //   })
-  // );
-
-  // await driver.get(
-  //   "https://www.booking.com/hotel/vn/old-town-resort-phu-quoc-phu-quoc.html"
-  // );
-
-  // const roomGalleryElement = await driver
-  //   .findElement(
-  //     By.xpath(
-  //       "/html/body/div[2]/div/div[6]/div[1]/div[1]/div[1]/div/div[2]/div[11]/div/div/div[1]"
-  //     )
-  //   )
-  //   .click();
-
   const room = await crawlService.hotelInfo();
-
   driver.quit();
-  console.log("room", JSON.stringify(room));
+  try {
+    await fs.writeFile("result.json", JSON.stringify(room));
+    console.log("file written successfully");
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 main();
