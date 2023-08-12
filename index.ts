bff1ac71e10cf30e8d4b29b847cf0616c5c1a958
PARADISE GAMING <dasf@gmail.com>
2023-08-12 16:41:04 +0700

    crawl data

--- index.ts
+++ index.ts
@@ -27,12 +27,12 @@ async function main() {
   //   "https://www.booking.com/hotel/vn/the-house-17-47-1.vi.html?aid=304142&label=gen173nr-1FCAEoggI46AdIM1gEaPQBiAEBmAExuAEHyAEM2AEB6AEB-AECiAIBqAIDuAKgw-CjBsACAdICJDhkMjU3ZmVlLTEzZGYtNDhmNC04MjNlLTM3M2Q5NjllZDgxMtgCBeACAQ&sid=cffea0f0aed14ebbecc74a7f095ddb8d&dest_id=-3714811;dest_type=city;dist=0;group_adults=2;group_children=0;hapos=147;hpos=22;no_rooms=1;req_adults=2;req_children=0;room1=A%2CA;sb_price_type=total;sr_order=popularity;srepoch=1685610457;srpvid=d0ba402b4e700228;type=total;ucfs=1&#hotelTmpl"
   // );F
   // await crawlAroundHotelError();
-  // await showResult();
+  await showResult();
   // await count();
 
   // crawlCityAreaList()
 
-  crawlCityArea()
+  // crawlCityArea()
 
   // const url = new URL(area)
 
